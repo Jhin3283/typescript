71f4dbbd97bd9ba22e56bc0b4fd48838b014ee0a
Jhin3283 <hj3283@gmail.com>
2022-04-22 13:14:33 +0900

    FIX:error

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const name = "Hyunjin",
+  age = 29,
+  gender = "male";
+
+const sayHi = (name: string, age: number, gender: string): string => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi(name, age, gender));
+export {};
