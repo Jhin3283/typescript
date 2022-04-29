3e4c4a6437d02af5ca708bc4ff00a6f10e714b85
Jhin3283 <hj3283@gmail.com>
2022-04-29 13:12:10 +0900

    study z

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-const name = "hyunjin",
-  age = 29,
-  gender = "male";
-
-const sayHi = (name: string, age: number, gender: string): string => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+const person = {
+  name: "hyunjin",
+  gender: "male",
+  age: 29,
+};
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(name, age, gender));
+console.log(sayHi(person));
 export {};
